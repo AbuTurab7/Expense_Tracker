b1d310b1969e179c18a0b44067f2048e65db2f53
Abu Turab <turababu989@gmail.com>
2026-04-16 22:54:09 +0530

    Added files

--- backend/models/Budget.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const budgetSchema = new mongoose.Schema({
-  userId: String,
-  amount: Number,
-});
-
-module.exports = mongoose.model("Budget", budgetSchema);
